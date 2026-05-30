95b750d2d83d91d2fc100ccc2765f40ef981cf73
alarconv-dev <alarconv7474@gmail.com>
2026-05-30 09:00:44 -0500

    nuevo commit

--- portafolio-web/src/data/proyectos.js
+++ portafolio-web/src/data/proyectos.js
@@ -20,13 +20,13 @@ export const listaProyectos = [
     titulo: "Dashboard de analíticas",
     descripcion: "Panel con gráficos en tiempo real consumiendo una API REST propia. Demuestra que sabes diseñar APIs, manejar datos y visualizarlos de forma clara.",
     tags: ["React.js", "Python", "SQL", "Node.js", "Git", "GitHub", "CSS3"],
-    linkGithub: "https://github.com/alarconv-dev/sinnombre"
+    linkGithub: "https://github.com/alarconv-dev/analytics-dashboard.git"
   },
   {
     id: 4,
     titulo: "API REST de inventario",
     descripcion: "API documentada para gestionar inventario de productos: endpoints para listar, crear, actualizar y eliminar. Ideal para mostrar conocimiento de backend puro",
     tags: ["c#", "Node.js", "SQL", "Git", "GitHub"],
-    linkGithub: "https://github.com/tu-usuario/fullstack-management-system"
+    linkGithub: "https://github.com/alarconv-dev/API-REST-inventario.git"
   }
 ];
