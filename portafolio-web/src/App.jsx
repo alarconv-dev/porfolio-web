b530bb1f85f0a404486aa41fbdf84af6924154de
alarconv-dev <alarconv7474@gmail.com>
2026-05-29 19:10:01 -0500

    feat: error mal intencionado

--- portafolio-web/src/App.jsx
+++ portafolio-web/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import Header from './components/header.jsx';
+import Hero from './components/hero.jsx';
+import Projects from './components/projects.jsx';
+import Skills from './components/skills.jsx';
 
 export default function App() {
   return (
